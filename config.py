19e9e04a6492b05601fa1200e3f7a887a7416453
aflian.mahyaruddin <aflian.mahyaruddin@satudental.com>
2023-04-06 02:40:59 +0700

    First Commit

---
+++ config.py
@@ -0,0 +1,19 @@
+## CONFIGURATION FILE
+
+# Bot session name
+session_name_bot = "sessions/bot" 
+
+# Follow this guide to obtain your API credentials: 
+# https://core.telegram.org/api/obtaining_api_id
+API_ID = 28479004
+API_HASH = "dcb0dc590e0f1aa395f5ad4b382d732e"
+
+# Write to @BotFather in Telegram to create a bot token
+BOT_TOKEN = "6242283355:AAGGXp376q5XoR4J4Ftg4nc8kYm8rnmkbuA"
+
+# The ID of the OpenAI model to use for generating responses.
+model_engine = "gpt-3.5-turbo"
+
+# Get your API_KEY by creating an OpenAi account and the go here: 
+# https://platform.openai.com/account/api-keys
+openai_key = "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"
